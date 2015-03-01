3594a60f2980a574859d1b31a2048aebb6e736f0
Calder-Ty <calder.ty@gmail.com>
2015-02-28 19:18:35 -0700

    Last Commit for the Night, Setting up my Own UI

--- App-2/ui.R
+++ App-2/ui.R
@@ -1,9 +1,14 @@
 shinyUI(fluidPage(
-  titlePanel("title panel"),
+  titlePanel("How to Use Shiney"),
   
   sidebarLayout( position = "right",
-    sidebarPanel( "sidebar panel"),
-    mainPanel("main panel")
+    sidebarPanel( "You Can get"),
+    mainPanel(h1("First level title"),
+              h2("Second level title"),
+              h3("Third level title"),
+              h4("Fourth level title"),
+              h5("Fifth level title"),
+              h6("Sixth level title"))
   )
   
 ))
