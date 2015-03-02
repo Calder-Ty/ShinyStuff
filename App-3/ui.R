687c7d73ac9c136f43a8017685a33cef872b2776
Calder-Ty <calder.ty@gmail.com>
2015-03-01 17:25:29 -0700

    Clean Up UI and Fix bugs

--- App-3/ui.R
+++ App-3/ui.R
@@ -2,18 +2,25 @@ shinyUI(fluidPage(
   titlePanel("censusVis"),
   
   sidebarLayout(
-    sidebarPanel( "create deomographic maps with information from the 
-                  2010 US Census",
-                  br(),
-                  selectInput("PercRace", label = h4("Choose a variable to display"),
-                              choices = list("Percent White" = 1, "Percent Black" = 2,
-                                             "Percent Hispanic"= 3, "Percent Asian" = 4)),
-                  br(),
-                  sliderInput("IntrestSlider", label = h4("Range of interest"),
-                              min = 0, max = 100, value = c(0,100)
-    ),
-    mainPanel(
-      ))
+    sidebarPanel( helpText("create deomographic maps with information from the 
+                  2010 US Census"),
+                  
+                  selectInput("PercRace", 
+                              label = "Choose a variable to display",
+                              choices = list("Percent White" = 1, 
+                                             "Percent Black" = 2,
+                                             "Percent Hispanic"= 3, 
+                                             "Percent Asian" = 4),
+                              selected = "Percent White"),
+                  
+                  sliderInput("IntrestSlider", 
+                              label = "Range of interest",
+                              min = 0, 
+                              max = 100, 
+                              value = c(0,100)
+    )),
+    
+    mainPanel("Here is the main panel")
   )
   
-)
+))
