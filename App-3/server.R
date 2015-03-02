4e4c8a7e33aaa338a92fa3ad6c4ab8f31c2ba908
Calder-Ty <calder.ty@gmail.com>
2015-03-01 19:07:43 -0700

    Coppied from Tutorial

--- App-3/server.R
+++ App-3/server.R
@@ -3,13 +3,16 @@ library(mapproj)
 source("helpers.r")
 counties <- readRDS("App-3/data/counties.rds")
 
-shinyServer(function(input, output) {
+# server.R
+
+shinyServer(
+  function(input, output) {
     
-  output$Text1 <- renderText({
-    paste("You have selected", input$PercRace)
-  })
-  output$Text2 <- renderText({
-    paste("You have chosen a range from", input$IntrestSlider[1],
-         "to", input$IntrestSlider[2])
-  })
-})
+    output$map <- renderPlot({
+      
+      percent_map( #some arguments )
+    })
+    
+  }
+)
+
