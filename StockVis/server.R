f30dbf81ddbab1c07597e2a0505e49efcf1116d7
calder-ty <calder.ty@gmail.com>
2015-04-26 10:34:18 -0600

    Make the app Reactive

--- StockVis/server.R
+++ StockVis/server.R
@@ -5,13 +5,15 @@ source("helpers.R")
 
 shinyServer(function(input, output) {
 
-  output$plot <- renderPlot({
-    data <- getSymbols(input$symb, src = "yahoo", 
-      from = input$dates[1],
-      to = input$dates[2],
-      auto.assign = FALSE)
-                 
-    chartSeries(data, theme = chartTheme("white"), 
+  dataInput <- reactive({
+    getSymbols(input$symb, src = "yahoo", 
+               from = input$dates[1],
+               to = input$dates[2],
+               auto.assign = FALSE)
+  })
+  
+  output$plot <- renderPlot({               
+    chartSeries(dataInput(), theme = chartTheme("white"), 
       type = "line", log.scale = input$log, TA = NULL)
   })
   
